82308dfe8bf4b7e45bf02118e4bf140b25f5e349
Helge Sverre <helge.sverre@gmail.com>
2024-04-29 18:57:27 +0200

    laravel 11 support

--- tests/SanityTests.php
+++ tests/SanityTests.php
@@ -7,6 +7,7 @@ use HelgeSverre\Telefonkatalog\DataSources\Opplysningen1890;
 it('can search Gulesider', function () {
     $lookup = new Gulesider();
     $person = $lookup->find('95965871');
+
     expect($person)->not()->toBeNull()
         ->and($person->name)->toBe('Helge Sverre Hessevik Liseth')
         ->and($person->address)->toBe('Vognstølen 29')
@@ -14,20 +15,19 @@ it('can search Gulesider', function () {
         ->and($person->postalCode)->toBe('5096')
         ->and($person->phone)->toBe('95965871')
         ->and($person->url)->toBe('https://www.gulesider.no/oppslag/77190505/person');
-
-    dd($person);
-
 });
 
 it('can search Gulesider by name', function () {
     $lookup = new Gulesider();
     $people = $lookup->search('helge sverre liseth');
+
     expect($people)->toBeCollection()->and($people)->toHaveCount(1);
 });
 
 it('can search Opplysningen 1881', function () {
     $lookup = new Opplysningen1881();
     $person = $lookup->find('95965871');
+
     expect($person)->not()->toBeNull()
         ->and($person->name)->toBe('Helge Sverre Hessevik Liseth')
         ->and($person->address)->toBe('Vognstølen 29')
@@ -40,12 +40,14 @@ it('can search Opplysningen 1881', function () {
 it('can search Opplysningen 1881 by name', function () {
     $lookup = new Opplysningen1881();
     $people = $lookup->search('helge sverre liseth');
+
     expect($people)->toBeCollection()->and($people)->isNotEmpty();
 });
 
 it('can search Opplysningen 1890', function () {
     $lookup = new Opplysningen1890();
     $person = $lookup->find('95965871');
+
     expect($person)->not()->toBeNull()
         ->and($person->name)->toBe('Helge Sverre Hessevik Liseth')
         ->and($person->address)->toBe('Vognstølen 29')
@@ -58,5 +60,6 @@ it('can search Opplysningen 1890', function () {
 it('can search Opplysningen 1890 by name', function () {
     $lookup = new Opplysningen1890();
     $people = $lookup->search('helge sverre liseth');
+
     expect($people)->toBeCollection()->and($people)->toHaveCount(1);
 });
